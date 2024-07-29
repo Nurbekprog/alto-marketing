59ef0a1af641841a845962b9d08e1269f044d031
Nurbekprog <nurprogdata@gmail.com>
2024-07-29 14:26:08 +0500

    section1-ended

--- src/components/section1/Section1.jsx
+++ src/components/section1/Section1.jsx
@@ -1,8 +1,11 @@
 import "./Section1.scss";
+import section1Bg from "../../assets/images/section1-bg.png";
 const Section1 = () => {
   return (
-    <div>
+    <div className="section1-out">
       <h1 className="section1-h1">The neighborhood pharmacy that comes to you.</h1>
+      <button className="section1-btn">Transfer in seconds →</button>
+      <img className="section1-bg-img" src={section1Bg} alt="section-bg" />
     </div>
   )
 }
